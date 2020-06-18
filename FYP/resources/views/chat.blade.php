03eb94b6a4cbb9b7b5c7ac823d4e68040c9f884e
ahmadizzman107 <32258713+ahmadizzman107@users.noreply.github.com>
2020-06-18 17:41:34 +0800

    Added chat.blade.php and stripe.blade.php

---
+++ FYP/resources/views/chat.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat Bot</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/assets/css/chat.min.css">
+</head>
+<body>
+    <script id="botmanWidget" src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/chat.js'></script>
+</body>
+</html>
